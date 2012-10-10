6660f24a0dbbd942ad743cb8e26f9daa5652ec71
Stas SUȘCOV <stas@net.utcluj.ro>
2012-10-10 17:30:26 +0300

    Added pdf generation part and email.

--- courseware-certificate.php
+++ courseware-certificate.php
@@ -29,7 +29,7 @@ Author URI: http://wordpress.org/extend/plugins/courseware-certificate/
 define( 'COURSEWARE_CERTIFICATE', '0.1' );
 
 /**
- * Main Courseware-certificate Class
+ * Courseware Certificate Class
  */
 class CoursewareCertificate {
     /**
@@ -52,7 +52,6 @@ class CoursewareCertificate {
       $group_id = reset( $group_id )->slug;
 
       $group_assignments = BPSP_Assignments::has_assignments( $group_id );
-      $grades = array();
 
       $completed = true;
 
@@ -62,10 +61,6 @@ class CoursewareCertificate {
           break;
         }
 
-        $grades[] = BPSP_Gradebook::load_grade_by_user_id(
-          $group_assignment->ID, $response->post_author
-        );
-
         $responded = get_post_meta( $group_assignment->ID, 'responded_author' );
         if ( !in_array( $response->post_author, $responded ) ) {
           $completed = false;
@@ -73,25 +68,87 @@ class CoursewareCertificate {
       }
 
       if ( $completed && $passed ) {
-        self::generate_certificate( $response->post_author, $group_id, $grades );
+        self::generate_certificate( $response->post_author, $group_id );
       }
     }
 
     /**
-     * Generate and email the certificate
+     * Generates the pdf
      */
-    function generate_certificate( $user_id, $group_id, $grades ) {
-      $group = groups_get_group( array( 'group_id' => $group_id ) );
-      $user = get_users( array( 'include' => $user_id ) );
-      var_dump(
-        $user,
-        $group,
-        $grades
+    function save_pdf( $svg, $user ) {
+      $tmp_pdf = sys_get_temp_dir() . '/certificate.pdf';
+      $pdf = new TCPDF( PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false );
+
+      $pdf->SetCreator( PDF_CREATOR );
+      $pdf->SetAuthor( 'BuddyPress Courseware' );
+      $pdf->SetTitle( 'Certificate for ' . $user->display_name );
+
+      $pdf->SetMargins( 0, 0, 0 );
+      $pdf->SetHeaderMargin(0);
+      $pdf->SetFooterMargin(0);
+      $pdf->AddPage();
+      $pdf->ImageSVG(
+        $file=$svg,
+        $x=0,
+        $y=0,
+        $w='',
+        $h='',
+        $link='',
+        $align='',
+        $palign='',
+        $border=0,
+        $fitonpage=false
       );
-      die();
-      // TODO: generate certificate in png/pdf
+      $pdf->Output( $tmp_pdf, 'F' );
+
+      return $tmp_pdf;
     }
 
+    /**
+     * Parses the svg file
+     */
+    function prepare_pdf( $user, $group ) {
+      $original_svg = get_stylesheet_directory() . '/certificate.svg';
+
+      if ( !file_exists( $original_svg ) ) {
+        $original_svg = dirname( __FILE__ ) . '/svg/certificate.svg';
+      }
+
+      $tmp_svg = sys_get_temp_dir() . '/certificate.svg';
+
+      $svg = file_get_contents( $original_svg );
+
+      $svg = str_replace( 'USER_FULL_NAME', $user->display_name, $svg );
+      $svg = str_replace( 'GROUP_FULL_NAME', $group->name, $svg );
+      $svg = str_replace( 'GROUP_DESCRIPTION', $group->description, $svg );
+      $svg = str_replace( 'GROUP_URL', get_site_url(), $svg );
+      $svg = str_replace( 'FULL_DATE', bpsp_get_date( date('Y-m-d H:i:s' ) ), $svg );
+
+      file_put_contents( $tmp_svg, $svg );
+
+      return self::save_pdf( $tmp_svg, $user );
+    }
+
+    /**
+     * Emails the certificate
+     */
+    function email_certificate( $email, $pdf ) {
+      wp_mail( $email, 'BuddyPress Courseware Certificate', '', '', array( $pdf ) );
+    }
+
+    /**
+     * Generates the certificate
+     */
+    function generate_certificate( $user_id, $group_id ) {
+      $group = groups_get_group( array( 'group_id' => $group_id ) );
+      $user = reset( get_users( array( 'include' => $user_id ) ) );
+
+      $certificate = self::prepare_pdf( $user, $group );
+      if ( self::email_certificate( $user->user_email, $certificate ) ) {
+        unlink( $certificate );
+        unlink( str_replace( '.pdf', '.svg', $certificate ) );
+      }
+    }
 }
 CoursewareCertificate::init();
 ?>
